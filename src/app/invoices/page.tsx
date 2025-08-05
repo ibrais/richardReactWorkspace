dcb069b9cfe967a337394bc799244772509a0749
Richard Ma <hokieswimmerrichard@gmail.com>
2025-08-05 16:55:08 -0400

    Added a basic form and a submit button

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -7,10 +7,18 @@ export default function Page() {
             amount: formData.get('amount'),
             status: formData.get('status'),
         }
-
         // mutate data
         // revalidate the cache
     }
 
-    return <form action={createInvoice}>...</form>
+    return <form action={createInvoice}>
+
+        <label>Enter the amount:
+            <input type="text" name="amount" id="amount" />
+        </label>
+        <label>Enter the status:
+            <input type="text" name="status" id="status" />
+        </label>
+        <button type="submit">Submit</button>
+    </form>
 }
